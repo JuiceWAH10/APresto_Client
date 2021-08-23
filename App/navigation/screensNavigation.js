3cb0697b766e37ea472307e0cf5bc01541757fca
JuiceWAH10 <jjamora121014@gmail.com>
2021-08-24 02:38:51 +0800

    initial commit; changed many lines from CRUD functions, untested; need new firebase config

---
+++ App/navigation/screensNavigation.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { createStackNavigator, createAppContainer } from "@react-navigation/stack";
+
+import QRCodeScanner from '../screens/QRCodeScanner';
+
+// Client Screens
+import ClientHomepage from '../screens/owners/clientHomepage';
+import ClientProductAdd from '../screens/owners/clientProduct/clientProductAdd';
+import ClientProductEdit from '../screens/owners/clientProduct/clientProductEdit';
+import ClientProductList from '../screens/owners/clientProduct/clientProductList';
+import ClientRewardAdd from '../screens/owners/clientReward/clientRewardAdd';
+import ClientRewardEdit from '../screens/owners/clientReward/clientRewardEdit';
+import ClientRewardList from '../screens/owners/clientReward/clientRewardList';
+import ClientSukiList from '../screens/owners/clientSuki/clientSukiList';
+import ClientEditProfile from '../screens/owners/clientProfile/clientEditProfile';
+
+const Stack = createStackNavigator();
+
+export default Screens = () => {
+  return(
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="clientHomepage" component={ClientHomepage} />        
+      <Stack.Screen name="QRCodeScanner" component={QRCodeScanner} />       
+      <Stack.Screen name="clientProductAdd" component={ClientProductAdd} />
+      <Stack.Screen name="clientProductEdit" component={ClientProductEdit} />
+      <Stack.Screen name="clientProductList" component={ClientProductList} />
+      <Stack.Screen name="clientRewardAdd" component={ClientRewardAdd} />
+      <Stack.Screen name="clientRewardEdit" component={ClientRewardEdit} />
+      <Stack.Screen name="clientRewardList" component={ClientRewardList} />
+      <Stack.Screen name="clientSukiList" component={ClientSukiList} />
+      <Stack.Screen name="clientEditProfile" component={ClientEditProfile} />
+    </Stack.Navigator>
+    
+  );
+}
